4d80e47777bb6f9d5215482ea54605b2ddf23115
wddqing <wddqing@gmail.com>
2020-12-31 20:37:19 +0800

    123

--- sdks/wechat/auth_test.go
+++ sdks/wechat/auth_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestSDK_Code2Session(t *testing.T) {
-	sdk := NewSDK("wx6e47a9b5e7a779d0", "3ac6a621a75cb8a88d3bb4d8059583fc")
+	sdk := NewSDK("2", "1")
 
 	resp, err := sdk.Code2Session(&Code2SessionReq{
 		JsCode: "123",
