15ba2c894f67e901cf30f396755ebd1124bc5a57
wddqing <wddqing@gmail.com>
2021-01-10 23:18:29 +0800

    add xgin

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.14
 
 require (
 	github.com/doug-martin/goqu/v9 v9.10.0
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
 	github.com/garyburd/redigo v1.6.2
+	github.com/gin-contrib/zap v0.0.1
+	github.com/gin-gonic/gin v1.6.3
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/stretchr/testify v1.4.0
---
+++ xgin/gin.go
@@ -0,0 +1,31 @@
+package xgin
+
+import (
+	"time"
+
+	ginzap "github.com/gin-contrib/zap"
+	"go.uber.org/zap"
+
+	"github.com/fvbock/endless"
+	"github.com/gin-gonic/gin"
+)
+
+type XGinEngine struct {
+	*gin.Engine
+}
+
+func (e *XGinEngine) Listen(addr string) error {
+	if err := endless.ListenAndServe(addr, e.Engine); err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewGin(logger *zap.Logger) *XGinEngine {
+	e := gin.Default()
+
+	e.Use(ginzap.Ginzap(logger, time.RFC3339, true))
+	e.Use(ginzap.RecoveryWithZap(logger, true))
+
+	return &XGinEngine{Engine: e}
+}
